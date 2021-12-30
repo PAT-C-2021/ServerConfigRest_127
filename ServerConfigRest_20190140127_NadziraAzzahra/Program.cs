823cf9616e24e6b8fbc0bbee4d170f53d4ab9ab3
Nadziraraa <nadziraazahra10@gmail.com>
2021-12-31 00:05:00 +0700

    Add project files.

---
+++ ServerConfigRest_20190140127_NadziraAzzahra/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceRest_20190140127_NadziraAzzahra;
+
+namespace ServerConfigRest_20190140127_NadziraAzzahra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server ready ..");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
